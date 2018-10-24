310a4a1de83cf17d33177cadb20e8defd5376507
jorgec <jorgec2013@gmail.com>
2018-10-24 11:05:29 -0400

    + Spark and details

--- spark/p3a.py
+++ spark/p3a.py
@@ -1,1 +1,6 @@
-#Code here
+#Commands to change col names, also for schema purposes
+#df11 = df1.toDF("regionEducativa","distritoEscolar ","municipio","Escuela_ID","Escuela_Nombre","nivel","CollegeBoard_ID ")
+#df22 = df2.toDF("region","distrito","eid","nombreEscuela","nivel","sexo","sid")
+
+
+df11.join(df22,'eid').filter(df11.nivel == "Superior").filter(df22.sexo == "M").filter((df11.municipio == "Ponce") | (df11.municipio == "San Juan")).show()
--- spark/p3b.py
+++ spark/p3b.py
@@ -1,1 +1,6 @@
-#Code here
+#Commands to change col names, also for schema purposes
+#df11 = df1.toDF("regionEducativa","distritoEscolar ","municipio","Escuela_ID","Escuela_Nombre","nivel","CollegeBoard_ID ")
+#df22 = df2.toDF("region","distrito","eid","nombreEscuela","nivel","sexo","sid")
+
+
+df11.filter(df11.regionEducativa == "Arecibo").groupBy(df11.distritoEscolar, df11.municipio).count().show()
